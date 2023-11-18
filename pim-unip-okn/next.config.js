93178ebba7f7ab50ed1bddbde2ae7467f9132655
Tiago Estetele <116907235+TiagoEstetele@users.noreply.github.com>
2023-11-18 16:55:25 -0300

    Update next.config.js

--- pim-unip-okn/next.config.js
+++ pim-unip-okn/next.config.js
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {};
 
-module.exports = nextConfig
+module.exports = {
+  webpackDevMiddleware: (config) => {
+    config.watchOptions = {
+      poll: 1000,
+      aggregateTimeout: 300,
+    };
+    return config;
+  },
+};
